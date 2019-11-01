3fb2d1021974f695e7fad89b86e29105c80312ff
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-11-01 14:50:45 +0100

    Update restic-backup.ps1

--- setup/restic-backup.ps1
+++ setup/restic-backup.ps1
@@ -2,19 +2,26 @@
 # .\restic_*.exe -r "restic-repo" restore latest --target "<TARGET>"
 # <PASSWORD>
 #
+# ask for password
+$passfile = "restic-pwd.txt"
+$pass = Read-Host 'What is your password?' -AsSecureString
+[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass)) > $passfile
 # create repository "restic-repo"
-.\restic_*.exe init -r "restic-repo"
-<PASSWORD>
-<PASSWORD>
+.\restic_*.exe init -r "restic-repo" --password-file  $passfile
 
 # backup <SOURCE> to respository
-.\restic_*.exe -r "restic-repo" --verbose backup "<SOURCE>"
-<PASSWORD>
+.\restic_*.exe -r "restic-repo" --verbose backup "<SOURCE>"  --password-file  $passfile
+
+# forget old backups
+# for the last 30 days, keep one each
+# for the last 12 months, keep one each
+.\restic_*.exe -r "restic-repo" forget --keep-daily 30 --keep-monthly 12 --password-file  $passfile
 
 # verify integrity
-.\restic_*.exe -r "restic-repo" check --read-data
-<PASSWORD>
+.\restic_*.exe -r "restic-repo" check --read-data --password-file  $passfile
 
 # list all snapshots in repository
-.\restic_*exe -r "restic-repo" snapshots
-<PASSWORD>
+.\restic_*exe -r "restic-repo" snapshots --password-file  $passfile
+
+# remove password file
+rm  $passfile

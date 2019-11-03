3faba67c5de6c34dc2f0a85a84eacd43f18ca8e5
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-11-03 20:58:48 +0100

    Update after-install.ps1
    sort choc packages

--- setup/after-install.ps1
+++ setup/after-install.ps1
@@ -13,43 +13,52 @@ ELSE
     Set-ExecutionPolicy Bypass -Scope Process
 }
 
-# install package manager
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# install commong software packages
-# sources: https://chocolatey.org/packages
-choco install notepadplusplus --confirm
-choco install 7zip
-choco install jre8
-choco install firefox
-choco install vlc
-choco install sysinternals
-choco install vcredist140 # Microsoft Visual C++ Redistributable for Visual Studio
-choco install foxitreader
-choco install pdfcreator
-choco install gimp
-choco install teamviewer
-choco install paint.net
-choco install thunderbird
-choco install keepass
-choco install libreoffice-fresh
-choco install youtube-dl
-choco install audacity
-choco install audacity-lame
-choco install winmerge
-choco install steam
-choco install mp3tag
-choco install texmaker
-choco install shutup10
-choco install patch-my-pc
-choco install freemind
-choco install unetbootin
-choco install tor-browser
-choco install veracrypt
-choco install signal
-choco install ublockorigin-firefox
-choco install noscript
-
 # remove all windows apps except calculator
 Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Store*" -and $_.name -notlike "*Calculator*"} | Remove-AppxPackage -erroraction silentlycontinue
 Get-AppxProvisionedPackage -online | where-object {$_.displayname -notlike "*Store*" -and $_.displayname -notlike "*Calculator*"} | Remove-AppxProvisionedPackage -online -erroraction silentlycontinue
+
+# install chocolatey package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+### install common software packages
+# sources: https://chocolatey.org/packages
+choco install vcredist140 # Microsoft Visual C++ Redistributable for Visual Studio
+# browser and addons:
+choco install firefox
+choco install ublockorigin-firefox
+choco install noscript
+choco install tor-browser
+# security:
+choco install shutup10
+choco install patch-my-pc
+choco install keepass
+choco install veracrypt
+# message communication:
+choco install signal
+choco install thunderbird
+# productivity tools:
+choco install notepadplusplus --confirm
+choco install 7zip
+choco install jre8
+choco install libreoffice-fresh
+choco install winmerge
+choco install mp3tag
+choco install freemind
+# media:
+choco install vlc
+choco install foxitreader
+choco install pdfcreator
+choco install gimp
+choco install paint.net
+choco install youtube-dl
+choco install audacity
+choco install audacity-lame
+# sysadmin tools:
+choco install teamviewer
+choco install sysinternals
+# choco install unetbootin
+# latex:
+# choco install miktex
+# choco install texmaker
+# gaming:
+# choco install steam

406a10c27a027d65829deabfe1939f69410f207c
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-11-09 12:33:34 +0100

    Create restic-backup.bat

---
+++ backup and restore/restic-backup.bat
@@ -0,0 +1,8 @@
+rem this script will backup the pictures and documents folders toa restic repo in OneDrive
+rem assumes: 
+rem (1) OneDrive is enabled with default folder settings
+rem (2) restic.exe exists in current directory
+rem (3) repository is created as "restic-repo"
+rem (3) restic-pwd.txt holds the repos password
+.\restic.exe -r "%userprofile%\OneDrive\restic-repo" backup "%userprofile%\pictures" "%userprofile%\documents" --password-file "restic-pwd.txt"
+.\restic.exe -r "%userprofile%\OneDrive\restic-repo" forget --keep-daily 30 --keep-monthly 12 --password-file "restic-pwd.txt"

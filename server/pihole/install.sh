18ecb97fdc062d4bcab9e36fa13818b314c9a922
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-11-26 21:50:01 +0100

    Update install.sh

--- server/pihole/install.sh
+++ server/pihole/install.sh
@@ -3,4 +3,4 @@ curl -O -L https://raw.githubusercontent.com/gXeeXqBHuHDFTaEnff3Z/blue-team-SOHO
 sudo mkdir /etc/pihole/
 sudo mv setupVars.conf /etc/pihole/setupVars.conf
 # start install using given settings
-sudo curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
+curl -L https://install.pi-hole.net | sudo bash /dev/stdin --unattended

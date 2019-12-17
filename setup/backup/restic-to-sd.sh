2fa806ac2d66d43de3f3b76230d97d7f59126fb8
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-12-17 21:35:16 +0100

    Update restic-to-sd.sh

--- setup/backup/restic-to-sd.sh
+++ setup/backup/restic-to-sd.sh
@@ -4,6 +4,9 @@ SD="/dev/mmcblk0"
 SOURCE="/var/www/nextcloud/data/$USER/files"
 
 echo "Backup of $SOURCE to $TARGET"
+# mount sd card
+mount -t vfat /dev/mmcblk0p1 /mnt/sdc1/
+
 
 TODO:
 https://restic.readthedocs.io/en/stable/080_examples.html#backing-up-your-system-without-running-restic-as-root

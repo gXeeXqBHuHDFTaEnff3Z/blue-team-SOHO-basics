442e7d4b96a24843d9ed1b8b3074ae734e8fce3b
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-12-27 20:03:41 +0100

    Delete ssh.sh

--- server/control node/ssh.sh
+++
@@ -1,9 +0,0 @@
-# copy etc files
-cp etc/hosts /etc/hosts
-cp etc/sudoers /etc/sudoers
-# generate public key
-ssh-keygen -b 4096
-# copy public key to server
-ssh-copy-id -i .ssh/key_rsa.pub chris@192.168.188.2
-# login via ssh key:
-ssh -i .ssh/key_rsa chris@192.168.188.2

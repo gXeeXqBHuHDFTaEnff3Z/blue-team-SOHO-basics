c12a0973ace9320b07439954fde60b6aececdf46
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2019-12-28 17:06:17 +0100

    Update nextcloudpi_docker_run.sh

--- server/nextcloudpi_docker_run.sh
+++ server/nextcloudpi_docker_run.sh
@@ -4,8 +4,9 @@ docker run -d \
     --name nextcloudpi \
     -p 80:80 \
     -p 443:443 \
+    -p 4443:4443 \
     -v "$(pwd)/data:/var/www/html/data" \
     -v "$(pwd)/config:/var/www/html/config" \
     --dns=127.0.0.1 \
     --restart=unless-stopped \
-    ownyourbits/nextcloudpi-x86:latest
+    ownyourbits/nextcloudpi-x86:latest 10.0.0.4

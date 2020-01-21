ae95315030be02abdcd9db65747345ee4049053f
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2020-01-21 14:40:32 +0100

    Delete nextcloud-install.sh

--- server/nextcloud-install.sh
+++
@@ -1,5 +0,0 @@
-# source: https://www.c-rieger.de/nextcloud-installation-guide-debian-9-10/
-# Change into sudo mode
-sudo -s
-# install nextcloudpi from https://ownyourbits.com/nextcloudpi/
-curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | bash

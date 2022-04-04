e343a36866293b43f1ee7da43868501e6e88330d
Christoph Weißenborn <20565781+gXeeXqBHuHDFTaEnff3Z@users.noreply.github.com>
2022-04-04 23:50:44 +0200

    Update usbhdd.sh

--- server/backup/usbhdd.sh
+++ server/backup/usbhdd.sh
@@ -11,4 +11,5 @@ df -h
 # create repo
 restic init --repo /media/usbhdd/
 # backup repo
-restic -r /media/usbhdd/ backup /var/lib/docker/volumes/nextcloud_data/_data/Chris/files/
+# OLD: restic -r /media/usbhdd/ backup /var/lib/docker/volumes/nextcloud_data/_data/Chris/files/
+restic -r /media/usbhdd/ backup /var/lib/docker/volumes/chris_data/_data/chris/files
